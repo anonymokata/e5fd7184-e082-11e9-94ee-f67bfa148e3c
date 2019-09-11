06cb68fda0ca437071ae19eb38b6373efe0d5550
someone <someone>
2019-09-11 19:45:21 -0400

    Added family class skeleton

---
+++ BabysitterKata.Core/Family.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BabysitterKata.Core {
+    public class PayEntry {
+        public TimeSpan StartTime { get; private set; }
+        public int Pay { get; private set; }
+
+        public PayEntry(TimeSpan startTime, int pay) {
+            this.StartTime = startTime;
+            this.Pay = pay;
+        }
+    }
+
+    public class Family {
+        public string Name { get; private set; }
+        public IReadOnlyList<PayEntry> PayScale { get; private set; }
+
+        private Family(string name, IReadOnlyList<PayEntry> payScale) {
+            this.Name = name;
+            this.PayScale = payScale;
+        }
+
+        //TODO This is better abstracted out into a data store
+        public static List<Family> GetFamilies() => throw new NotImplementedException();
+    }
+}
