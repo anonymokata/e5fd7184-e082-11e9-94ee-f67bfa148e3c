fa28a894d8e4a31c82bfad2f8f4765e19a12385c
someone <someone>
2019-09-11 18:53:50 -0400

    Add project skeleton

---
+++ BabysitterKata.Cli/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace BabysitterKata.Cli {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ BabysitterKata.Core/Class1.cs
@@ -0,0 +1,6 @@
+﻿using System;
+
+namespace BabysitterKata.Core {
+    public class Class1 {
+    }
+}
