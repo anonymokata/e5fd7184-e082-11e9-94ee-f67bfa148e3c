c59bb71dfd5013dc3fcab05473e8d0b5cea9357c
someone <someone>
2019-09-11 19:37:47 -0400

    Switched to const for start/end time

--- BabysitterKata.Core/Babysitter.cs
+++ BabysitterKata.Core/Babysitter.cs
@@ -2,6 +2,9 @@
 
 namespace BabysitterKata.Core {
     public class Babysitter {
+        public static TimeSpan DefaultStartTime { get; } = new TimeSpan(17, 0, 0);
+        public static TimeSpan DefaultEndTime { get; } = new TimeSpan(4, 0, 0);
+
         public Babysitter(TimeSpan startTime, TimeSpan endtime) {
 
         }
--- BabysitterKata.Tests/InvalidHours.cs
+++ BabysitterKata.Tests/InvalidHours.cs
@@ -8,7 +8,7 @@ namespace BabysitterKata.Tests {
     public class InvalidHours {
         private readonly Babysitter sitter;
 
-        public InvalidHours() => this.sitter = new Babysitter(new TimeSpan(17, 0, 0), new TimeSpan(4, 0, 0));
+        public InvalidHours() => this.sitter = new Babysitter(Babysitter.DefaultStartTime, Babysitter.DefaultEndTime);
 
         [TestMethod]
         public void EndComesBeforeStartThrows() {
