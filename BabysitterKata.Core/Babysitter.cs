b6b1e985076f2af647a308b99207f688a8b00f7a
someone <someone>
2019-09-11 20:45:57 -0400

    Store start and end time for babysitter

--- BabysitterKata.Core/Babysitter.cs
+++ BabysitterKata.Core/Babysitter.cs
@@ -2,12 +2,16 @@
 
 namespace BabysitterKata.Core {
     public class Babysitter {
+        private readonly TimeSpan startTime;
+        private readonly TimeSpan endTime;
+
         //TODO These are better stored in a data store
         public static TimeSpan DefaultStartTime { get; } = new TimeSpan(17, 0, 0);
         public static TimeSpan DefaultEndTime { get; } = new TimeSpan(4, 0, 0);
 
         public Babysitter(TimeSpan startTime, TimeSpan endtime) {
-
+            this.startTime = startTime;
+            this.endTime = endtime;
         }
 
         public int CalculatePay(Family family, TimeSpan startTime, TimeSpan endtime) {
